25330a5e819014cc587901bf1b8ca843ad39790a
meskalilbarbaro <147388714+meskalilbarbaro@users.noreply.github.com>
2024-03-23 19:45:03 +0100

    Update MultFunctionWrap.hpp

--- MultFunctionWrap.hpp
+++ MultFunctionWrap.hpp
@@ -6,7 +6,9 @@ class MultFunctionWrap{
 private:
     std::vector<FunctionWrap> function_wrap_v;
 public:
+    //constructor
     MultFunctionWrap(const std::vector<std::string> &expr_v, size_t size);
+    //Evaluation operator
     std::vector<double> operator()(const std::vector<double> &x);
 };
 #endif //CHALLENGE1PACS_MULTFUNCTIONWRAP_HPP
